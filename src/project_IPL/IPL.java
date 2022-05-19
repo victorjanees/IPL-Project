492ee6dd682024ea339451536a3332463492c777
victorjanees <victorjanees@gmail.com>
2022-05-19 13:09:24 +0530

    Added CSV Reader

--- src/project_IPL/IPL.java
+++ src/project_IPL/IPL.java
@@ -1,10 +1,25 @@
 package project_IPL;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io. *;
 
 public class IPL {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String matches = "/home/victor/Downloads/matches.csv";
+	    String deliveries ="/home/victor/Downloads/deliveries.csv";
+	    String line = "";
+	    
+	    try {
+	    BufferedReader mReader = new BufferedReader(new FileReader(matches));
+	    BufferedReader dReader = new BufferedReader(new FileReader(deliveries));
+	    while ((line = mReader.readLine()) != null ) {
+	    	System.out.println(line);
+	    }
+	    }
+	    catch (Exception e) {
+	    	e.printStackTrace();
+	    }
 	}
 
 }
