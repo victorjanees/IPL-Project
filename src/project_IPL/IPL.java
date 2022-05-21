de0aadfbca742dfee34db522ce38df10368c21d8
victorjanees <victorjanees@gmail.com>
2022-05-21 12:05:23 +0530

    scenario-2 done

--- src/project_IPL/IPL.java
+++ src/project_IPL/IPL.java
@@ -19,7 +19,9 @@ public class IPL {
 	    BufferedReader mReader = new BufferedReader(new FileReader(matches));
 	    BufferedReader dReader = new BufferedReader(new FileReader(deliveries));
 	    HashMap<String,Integer> matches_per_season = new HashMap<String,Integer>();
+	    HashMap<String,Integer> wins_by_teams = new HashMap<String,Integer>();
 	    int matches_played=1;
+	    int matches_won=1;
 	    while ((line = mReader.readLine()) != null) {
 	    	String [] values = line.split(",");
 	    	if (matches_per_season.get(values[1])==null) {
@@ -28,11 +30,20 @@ public class IPL {
 	    	}else {
 	    	matches_played = matches_played +1;
 	    	matches_per_season.put(values[1], matches_played);
+	    	}
+	        if (wins_by_teams.get(values[10])==null) {
+	    	matches_won=1;
+	    	wins_by_teams.put(values[10], matches_won);
+	    	}else {
+	    		matches_won=matches_won+1;
+	    		wins_by_teams.put(values[10], matches_won);
 	    	}}
 	    System.out.println("Scenario:1");
 	    System.out.println("Total matches per each year = " + matches_per_season);
+	    System.out.println("Scenario:2");
+	    System.out.println("Total matches won by all teams year = " + wins_by_teams);
+	}
 	    
-	    }
 	    catch (Exception e) {
 	    	e.printStackTrace();
 	    }
