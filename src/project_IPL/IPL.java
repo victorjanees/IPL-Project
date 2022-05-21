ea1a1b3db649869dba2dfd7e29d2a0a44d8846c4
victorjanees <victorjanees@gmail.com>
2022-05-21 10:28:28 +0530

    scenario-1 done

--- src/project_IPL/IPL.java
+++ src/project_IPL/IPL.java
@@ -1,25 +1,43 @@
 package project_IPL;
-import java.io.BufferedReader;
+import java.util.HashMap;
 import java.io.FileReader;
 import java.io. *;
 
 public class IPL {
+	
+	public static void matchesPerYear() {
+		
+	}
 
 	public static void main(String[] args) {
 		String matches = "/home/victor/Downloads/matches.csv";
 	    String deliveries ="/home/victor/Downloads/deliveries.csv";
 	    String line = "";
 	    
+	    
 	    try {
 	    BufferedReader mReader = new BufferedReader(new FileReader(matches));
 	    BufferedReader dReader = new BufferedReader(new FileReader(deliveries));
-	    while ((line = mReader.readLine()) != null ) {
-	    	System.out.println(line);
-	    }
+	    HashMap<String,Integer> matches_per_season = new HashMap<String,Integer>();
+	    int matches_played=1;
+	    while ((line = mReader.readLine()) != null) {
+	    	String [] values = line.split(",");
+	    	if (matches_per_season.get(values[1])==null) {
+	    	matches_played=1;
+	    	matches_per_season.put(values[1], matches_played);  		
+	    	}else {
+	    	matches_played = matches_played +1;
+	    	matches_per_season.put(values[1], matches_played);
+	    	}}
+	    System.out.println("Scenario:1");
+	    System.out.println("Total matches per each year = " + matches_per_season);
+	    
 	    }
 	    catch (Exception e) {
 	    	e.printStackTrace();
 	    }
+	    
+	    
 	}
 
 }
